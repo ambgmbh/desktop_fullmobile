c08224bdff2f543633fdfb099c0b6993ea8c9566
Edgar Wahl <ewahl64@googlemail.com>
2019-02-13 20:11:11 +0100

    Initial Commit

---
+++ desktop_fullmobile/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  desktop_fullmobile
+//
+//  Created by admin on 13.02.19.
+//  Copyright © 2019 AMB Kennzeichenzentrale GMBH. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
